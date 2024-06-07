086c0b24f4a8aa9e2d167ff34b1f0bb6da62f0eb
saadat <saadat0989@gmail.com>
2024-06-08 01:02:38 +0600

    vacanciepage UI buttum

--- src/pages/Vacancies/Vacancies.tsx
+++ src/pages/Vacancies/Vacancies.tsx
@@ -1,21 +1,57 @@
-import Footer from "../../Companent/Footer/Footer"
-import Header from "../../Companent/Header/Header"
-
+import { Link } from "react-router-dom";
+import Footer from "../../Companent/Footer/Footer";
+import Header from "../../Companent/Header/Header";
+import "./Vacancies.css";
 
 function Vacancies() {
   return (
     <>
-    <Header/>
-    <div id="vacancies">
+      <Header />
+      <div id="vacancies">
         <div className="container">
-            <div className="vacancies">
-            vacancies
+          <div className="vacancies">
+            <div className="vacancies__content">
+              <div className="filter">
+                <Link to={"/addvacancie"}>
+                  <button className="add_vacancies">Добавить вакансию</button>
+                </Link>
+              </div>
+              <div className="jobs-list">
+                <div className="jobs">
+                  <a
+                    href="https://devkg.com/ru/jobs/backend-c-developer-ooo-altideyta-12830"
+                    className="link-vacancies"
+                  >
+                    <div className="information-vacancies">
+                      <div className="company-icons">
+                        <div className="icon"></div>
+                      </div>
+                      <div className="jobs-item-filed">
+                        <span className="label">Компания</span> ООО АЛТИДЭЙТА
+                      </div>
+                      <div className="jobs-item-filed">
+                        <span className="label">Должность</span>
+                        Backend C# Developer
+                      </div>
+                      <div className="jobs-item-filed">
+                        <span className="label">Оклад</span>
+                        100000 - 200000 KGS в месяц
+                      </div>
+                      <div className="jobs-item-filed">
+                        <span className="label">Тип</span>
+                        Удаленная работа
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Vacancies
+export default Vacancies;
