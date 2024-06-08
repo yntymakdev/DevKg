041a069117e443786f4f81119fccba33c952a673
uzenbaeva <akmaralzamirbekova35@gmail.com>
2024-06-08 14:36:04 +0600

    first commit

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,18 +1,33 @@
 import Footer from "../../Companent/Footer/Footer"
 import Header from "../../Companent/Header/Header"
+import Automotion from "./Automation/automation"
+import Awards from "./Awards/awards"
+import Battle from "./Battle/battle"
+import Expo from "./Expo/expo"
+import Kitforum from "./KitForum/kitforum"
+import Sql from "./SQL/sql"
 
+import "./Events.css"
 
 function Events() {
   return (
     <>
       <Header/>
-    <div id="events">
-        <div className="container">
-            <div className="events">
-            events
+        <div id="events">
+            <div className="container">
+                <div className="events">
+                  <div className="events__button">
+                    <button>Добавить мероприятие</button>
+                  </div>
+                </div>
             </div>
         </div>
-    </div>
+        <Sql/>
+        <Automotion/>
+        <Battle/>
+        <Kitforum/>
+        <Awards/>
+        <Expo/>
     <Footer/>
     </>
   )
