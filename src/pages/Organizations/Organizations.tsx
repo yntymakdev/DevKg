07b0eb611fcb1c7938d02637913cb5e5356a96bd
seyitkulov <islamsejtkulov@gmail.com>
2024-06-18 10:27:37 +0600

    Organizatios bytty

--- src/pages/Organizations/Organizations.tsx
+++ src/pages/Organizations/Organizations.tsx
@@ -1,21 +1,46 @@
-import Footer from "../../Companent/Footer/Footer"
-import Header from "../../Companent/Header/Header"
-
+import Footer from "../../Companent/Footer/Footer";
+import Header from "../../Companent/Header/Header";
+import "../Organizations/Organizations.css";
+import organizationsImg from "../../assets/Images/53fecf5935b721031f4f636b13bf18ae.webp";
 
 function Organizations() {
-  return (
-    <>
-      <Header/>
-    <div id="organizations">
-        <div className="container">
-            <div className="organizations">
-            organizations
+    return (
+        <>
+            <Header />
+            <div id="organizations">
+                <div className="container">
+                    <div className="organizations">
+                        <div className="organizations-logo-block">
+                            <div className="organizations-img">
+                                <img src={organizationsImg} alt="" />
+                            </div>
+                            <div className="organizations-logo">
+                                <h5>
+                                    Компания
+                                    <h4>CodifyLab</h4>
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="organizations-info">
+                            <div className="organizations-info-vacancies">
+                                <h5>Ваканции</h5>
+                                <h4>193</h4>
+                            </div>
+                            <div className="organizations-info-events">
+                                <h5>Мероприятия</h5>
+                                <h4>100</h4>
+                            </div>
+                            <div className="organizations-info-video">
+                                <h5>Видео</h5>
+                                <h4>0</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <Footer/>
-    </>
-  )
+            <Footer />
+        </>
+    );
 }
 
-export default Organizations
+export default Organizations;
