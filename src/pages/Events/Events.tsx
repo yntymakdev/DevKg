66bd6982a35296befd3e1238d3e050fe378d7ac9
uzenbaeva <akmaralzamirbekova35@gmail.com>
2024-07-04 17:25:08 +0600

    events finishing

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,14 +1,12 @@
 import Footer from "../../Companent/Footer/Footer"
 import Header from "../../Companent/Header/Header"
 import "./Events.css"
+import useFetch from "../../hooks/useFetch."
 import { EventsList } from "../../Companent/EventsList/eventsList"
-import useFetch from "../../hooks/useFetch"
 
 function Events() {
-  const{ data, isLoading } = useFetch({url: "http://3.38.98.134/events"})
+  const{ data, isLoading } = useFetch({url: "http://3.34.200.34/events"})
     console.log("data: ", data);
-
-    
     if(isLoading) {
         return <div>Loading...</div>
     }
@@ -31,17 +29,15 @@ function Events() {
                           eventsName={item?.name}
                           organizationName ={item?.organization_name}
                           locations = {item?.location}
-                      />
+                      />      
                     )
                   }    
-
-                </div>
+              </div>
+            </div>
             <div className="btns">
               <button className="btn previous">Предыдующая страница</button>
               <button className="btn next">Cледующая страница</button>
             </div>
-            </div>
-            
         </div>    
         <Footer/>
     </>
