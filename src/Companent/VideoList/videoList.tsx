4ed8cc7b39dbef179a7f8f1281cdd2ff037e90b6
uzenbaeva <akmaralzamirbekova35@gmail.com>
2024-07-19 12:04:03 +0600

    finished adaptation

--- src/Companent/VideoList/videoList.tsx
+++ src/Companent/VideoList/videoList.tsx
@@ -1,41 +1,43 @@
 export const VideoList = ({
-    imgUrl,
-    videoTitle,
-    date,
-    organizationName,
-    
+  organization_name,
+  title,
+  date,
+  cover,
 }: {
-    imgUrl: string,
-    videoTitle: number,
-    date: string,
-    organizationName: string,
-
+  organization_name: string;
+  title: string;
+  date: number;
+  cover: any;
 }) => {
-    return(
+  return (
     <>
-        <div className="video-block">
-            <div className="main-block">
-                <div className="first__block">
-                    <img src={imgUrl} alt="img" />
-                    <div className="first__block-title">
-                        <h2>
-                            {" "}
-                            {videoTitle}
-                        </h2>
-                    </div>
-                  <br />
-                    <div className="first__block-organizer">
-                        <span>Организатор</span>
-                            <h3>{organizationName}</h3>
-                    </div>
-                    <div className="first__block-data">
-                        <span>Когда</span>
-                            <br />
-                        <h3>{date}</h3>
-                    </div>
-                </div>
+      <div className="meetups-page-items">
+        <article className="meetups-wrapper item">
+          <a
+            href="/ru/meetups/chatgpt-revolyuciya-ili-moda-kak-neyroseti-mogut-pomoch-v-rabote-kolesa-group-138"
+            className="meetups-item"
+          >
+            <div className="preview dark image">
+              <picture>
+                <img src={cover} alt="" />
+              </picture>
             </div>
-        </div>
+            <div className="meta">
+              <div data-v-4ff14bbb className="title">{title}</div>
+              <div className="meetups-info">
+                <div className="organizer">
+                  <span className="label">Организатор</span>
+                  {organization_name}
+                </div>
+                <div className="date">
+                  <span className="label">Когда</span>
+                  <time dateTime="2023-09-12T06:10:05.6742">{date}</time>
+                </div>
+              </div>
+            </div>
+          </a>
+        </article>
+      </div>
     </>
-    )
-}
+  );
+};
