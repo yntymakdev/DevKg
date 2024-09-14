c129608f97fb962f5f88654a067a00d58026bcfa
unknown <ynty>
2024-09-14 15:44:32 +0600

    a

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,27 +1,21 @@
 import { useEffect, useState } from "react";
 
-const useFetch = ({
-  url = "http://3.38.98.134/jobs"
-}: {
-  url: string
-}) => {
-
-  const [data, setData] = useState <any> ([]);
+const useFetch = ({ url = "http://3.38.98.134/jobs" }: { url: string }) => {
+  const [data, setData] = useState<any>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
+  // aa
   const fetchData = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
       const response = await fetch(url);
       const data = await response.json();
-      if (data.statusCode = 200){
-        setData(data.data)
+      if ((data.statusCode = 200)) {
+        setData(data.data);
       }
-    }
-    catch (error){
-      console.error('Error fetching data:', error);
-    }finally{
-        setIsLoading(false)
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -33,6 +27,6 @@ const useFetch = ({
     isLoading,
     data,
   };
-}
+};
 
 export default useFetch;
